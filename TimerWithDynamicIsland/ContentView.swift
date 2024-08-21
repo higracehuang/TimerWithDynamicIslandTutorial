031d1682f4c0bdf9375397e3a684f34f74f85378
higracehuang <higracehuang@gmail.com>
2024-08-21 13:42:36 -0500

    Initial Commit

---
+++ TimerWithDynamicIsland/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimerWithDynamicIsland
+//
+//  Created by Le Huang on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimerWithDynamicIsland/TimerWithDynamicIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWithDynamicIslandApp.swift
+//  TimerWithDynamicIsland
+//
+//  Created by Le Huang on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerWithDynamicIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
